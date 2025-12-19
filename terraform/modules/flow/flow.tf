1756f83804c2ec4c9c0a9d49ce132eb44d9b7977
NMSanti <Nikko_Mark.Santillan@genesys.com>
2025-12-19 16:06:00 +0800

    Remove file_content_hash

--- terraform/modules/flow/flow.tf
+++ terraform/modules/flow/flow.tf
@@ -1,6 +1,5 @@
 resource "genesyscloud_flow" "Inbound_Message_Flow" {
   filepath          = "${path.module}/mobile-sdk-inbound-message-flow.yaml"
-  file_content_hash = filesha256("${path.module}/mobile-sdk-inbound-message-flow.yaml")
   substitutions = {
     workflow_name = var.workflow_name
     division      = var.division
